146266669e166b933df7a4f38eea13a4b4094759
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-11-01 19:49:52 +0700

    Update 1206.cpp

--- 1206.cpp
+++ 1206.cpp
@@ -2,6 +2,37 @@
 // Created by khanh on 10/8/2021.
 //
 
+/*
+ĐƯỜNG ĐI HAMILTON
+
+Bài làm tốt nhất
+Đường đi đơn trên đồ thị có hướng hoặc vô hướng đi qua tất cả các đỉnh của đồ thị mỗi đỉnh đúng một lần được gọi là đường đi Hamilton. Cho đồ thị vô hướng G = , hãy kiểm tra xem đồ thị có đường đi Hamilton hay không?
+
+Input:
+
+Dòng đầu tiên đưa vào số lượng bộ test T.
+Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm hai phần: phần thứ nhất đưa vào hai số V, E tương ứng với số đỉnh, số cạnh của đồ thị; phần thứ hai đưa vào các cạnh của đồ thị.
+T, V, E thỏa mãn ràng buộc: 1≤T ≤100; 1≤V≤10; 1≤ E ≤15.
+Output:
+
+Đưa ra 1 hoặc 0 tương ứng với test có hoặc không có đường đi Hamilton theo từng dòng.
+Ví dụ:
+
+Input
+
+2
+4 4
+1 2 2 3 3 4 2 4
+4 3
+1 2 2 3 2 4
+
+Output
+
+1
+
+0
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;

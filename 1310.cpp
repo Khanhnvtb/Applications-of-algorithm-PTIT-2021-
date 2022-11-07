6bf355d11b90c234802460f65c908850c11d5fe3
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-11-07 19:15:05 +0700

    Update 1310.cpp

--- 1310.cpp
+++ 1310.cpp
@@ -37,25 +37,34 @@ Output
 
 using namespace std;
 
+int n, k, a[1005];
+
+void input() {
+    scanf("%d %d", &n, &k);
+    for (int i = 1; i <= k; i++) scanf("%d", &a[i]);
+}
+
+void solve() {
+    int i;
+    for (i = k; i > 0; i--) {
+        if (a[i] != n - k + i) {
+            a[i]++;
+            for (int j = i + 1; j <= k; j++)
+                a[j] = a[i] + j - i;
+            break;
+        }
+    }
+    if (i == 0) for (i = 1; i <= k; i++) printf("%d ", i);
+    else for (i = 1; i <= k; i++) printf("%d ", a[i]);
+    printf("\n");
+}
+
 int main() {
     int t;
     scanf("%d", &t);
     while (t--) {
-        int n, k;
-        scanf("%d %d", &n, &k);
-        int a[k + 1], i;
-        for (i = 1; i <= k; i++) scanf("%d", &a[i]);
-        for (i = k; i > 0; i--) {
-            if (a[i] != n - k + i) {
-                a[i]++;
-                for (int j = i + 1; j <= k; j++)
-                    a[j] = a[i] + j - i;
-                break;
-            }
-        }
-        if (i == 0) for (i = 1; i <= k; i++) printf("%d ", i);
-        else for (i = 1; i <= k; i++) printf("%d ", a[i]);
-        printf("\n");
+        input();
+        solve();
     }
     return 0;
 }

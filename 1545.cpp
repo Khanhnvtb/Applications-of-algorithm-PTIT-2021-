b83e0d2856bcd06645f35e6d122f080c869f054c
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-11-07 18:33:18 +0700

    Add files via upload

--- 1545.cpp
+++ 1545.cpp
@@ -1,61 +1,94 @@
 //
-// Created by khanh on 10/12/2021.
+// Created by khanhnvtb on 11/7/2022.
 //
 
-#include <iostream>
-#include <stdio.h>
-using namespace std;
-const int nmax=10e4+9;
-int IT[4*nmax];
-int a[nmax];
-int b[nmax]={};
-int n,m,q;
+/*
+GIÁ TRỊ LỚN NHẤT
 
-void MakeIT(int id, int l ,int r)
-{
-    int mid;
-    if (l>r)
+Cho dãy số gồm N phần tử có giá trị ban đầu bằng 0.
+Có M phép biến đổi, mỗi phép có dạng (u, v, k): tăng mỗi phần tử từ u tới v lên k đơn vị.
+Cho Q câu hỏi, mỗi câu hỏi có dạng (u, v): yêu cầu tìm phần tử lớn nhất trong đoạn [u, v].
+
+Input
+Dòng đầu tiên gồm 2 số nguyên N và M (1 <= N, M <= 100 000).
+M dòng tiếp theo, mỗi dòng gồm 3 số u, v, k (1 <= k <= 10 000).
+Dòng tiếp theo là số nguyên Q (1 <= Q <= 100 000).
+Q dòng tiếp theo, mỗi dòng gồm 2 số nguyên u, v.
+Output
+Với mỗi truy vấn, hãy in ra đáp án trên một dòng.
+Test ví dụ:
+
+Input
+
+6 2
+
+1 3 2
+
+4 6 3
+
+1
+
+3 4
+
+Output
+
+3
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int val, lazy;
+};
+
+int n, m;
+Node st[4 * 100005];
+
+void down(int id) {
+    int k = st[id].lazy;
+    st[id * 2].val += k;
+    st[id * 2].lazy += k;
+    st[id * 2 + 1].val += k;
+    st[id * 2 + 1].lazy += k;
+    st[id].lazy = 0;
+}
+
+void update(int id, int l, int r, int u, int v, int k) {
+    if (u > r || v < l) return;
+    if (u <= l && v >= r) {
+        st[id].val += k;
+        st[id].lazy += k;
         return;
-    if (l==r)
-    {
-        IT[id]=a[l];
-        return ;
     }
-    mid = (l+r)/2;
-    MakeIT(id*2,l,mid);
-    MakeIT(id*2+1,mid+1,r);
-    IT[id]=max(IT[id*2],IT[id*2+1]);
+    down(id);
+    int m = (l + r) / 2;
+    update(id * 2, l, m, u, v, k);
+    update(id * 2 + 1, m + 1, r, u, v, k);
+    st[id].val = max(st[id * 2].val, st[id * 2 + 1].val);
 }
-int Query(int id, int l, int r, int u, int v)
-{
-    if (r<u || l>v)
-        return -10e8;
-    if (u<=l && r<=v)
-        return(IT[id]);
-    int mid = (l+r)/2;
-    return (max (Query (id*2,l,mid,u,v) , Query(id*2+1,mid+1,r,u,v) ) ) ;
+
+int get(int id, int l, int r, int u, int v) {
+    if (u > r || v < l) return INT_MIN;
+    if (u <= l && v >= r) return st[id].val;
+    down(id);
+    int m = (l + r) / 2;
+    return max(get(id * 2, l, m, u, v), get(id * 2 + 1, m + 1, r, u, v));
 }
-int main()
-{
-    scanf("%d%d",&n,&m);
-    int u,v,k;
-    for (int i=1;i<=m;i++)
-    {
-        scanf("%d%d%d",&u,&v,&k);
-        b[u]+=k;
-        b[v+1]-=k;
+
+int main() {
+    scanf("%d %d", &n, &m);
+    int u, v, k;
+    while (m--) {
+        scanf("%d %d %d", &u, &v, &k);
+        update(1, 1, n, u, v, k);
     }
-    int x=0;
-    for (int i=1;i<=n;i++)
-    {
-        x+=b[i];
-        a[i]=x;
+    int q;
+    scanf("%d", &q);
+    while (q--) {
+        scanf("%d %d", &u, &v);
+        printf("%d\n", get(1, 1, n, u, v));
     }
-    MakeIT(1,1,n);
-    scanf("%d",&q);
-    for (int i=1;i<=q;i++)
-    {
-        scanf("%d%d",&u,&v);
-        printf("%d\n",Query(1,1,n,u,v));
-    }
+    return 0;
 }

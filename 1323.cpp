e8b5ca2fd2981d74f92c9a6b45bb21e94045deec
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-11-08 12:17:44 +0700

    Update 1323.cpp

--- 1323.cpp
+++ 1323.cpp
@@ -2,6 +2,32 @@
 // Created by khanh on 10/11/2021.
 //
 
+/*
+SỐ KHỐI LẬP PHƯƠNG
+Một số X được gọi là số khối lập phương nếu X là lũy thừa bậc 3 của số Y (X= Y3). Cho số nguyên dương N, nhiệm vụ của bạn là tìm số khối lập phương lớn nhất bằng cách loại bỏ đi các chữ số của N. Ví dụ số 4125 ta có kết quả là 125 = 53.
+
+Input:
+
+Dòng đầu tiên đưa vào số lượng bộ test T.
+Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test là một số tự nhiên N được viết trên một dòng.
+T, N thỏa mãn ràng buộc: 1≤T≤100;  1≤N≤1018.
+Output:
+
+Đưa ra kết quả mỗi test theo từng dòng. Nếu không tìm được đáp án in ra -1.
+Ví dụ:
+
+Input
+
+2
+4125
+976
+
+Output
+
+125
+-1
+*/
+
 #include<bits/stdc++.h>
 
 using namespace std;

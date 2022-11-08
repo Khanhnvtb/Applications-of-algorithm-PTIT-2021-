41cee8ccb9f92253db885d301d1f12ca76389fbb
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-11-08 12:44:48 +0700

    Update 1422.cpp

--- 1422.cpp
+++ 1422.cpp
@@ -2,6 +2,34 @@
 // Created by Khanh Nguyen Van on 10/16/2021.
 //
 
+/*
+XÂY DỰNG LẠI CÂY NHỊ PHÂN TÌM KIẾM
+Cho một mảng là phép duyệt level-order của cây nhị phân tìm kiếm. Nhiệm vụ của bạn là xây dựng lại cây nhị phân tìm kiếm bảo toàn được cấu trúc cây nhị phân ban đầu.
+
+     Input:
+
+Dòng đầu tiên đưa vào số lượng test T.
+Những dòng tiếp theo đưa vào các bộ test. Mỗi bộ test gồm dòng: dòng đầu tiên đưa vào số N là số lượng node của cây tìm kiếm; dòng tiếp theo đưa vào phép duyệt level-order của cây tìm kiếm; các số được viết cách nhau một vài khoảng trống.
+T, N, node thỏa mãn ràng buộc: 1≤T≤100; 1≤N≤103; 1≤node≤104;
+Output:
+
+Đưa ra kết quả mỗi test theo từng dòng là phép duyệt Inorder của cây tìm kiếm.
+      Ví dụ:
+
+Input
+
+2
+9
+7 4 12 3 6 8 1 5 10
+6
+1 3 4 6 7 8
+
+Output
+
+7 4 3 1 6 5 12 8 10
+1 3 4 6 7 8
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;

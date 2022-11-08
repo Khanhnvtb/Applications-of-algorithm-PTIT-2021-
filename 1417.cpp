edfb194cef2af268370f168255fb0432e2219972
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-11-08 12:34:49 +0700

    Update 1417.cpp

--- 1417.cpp
+++ 1417.cpp
@@ -74,16 +74,16 @@ string isPerfectBinaryTree(Node *node) {
         q.pop();
         if (!tmp->left && !tmp->right) {
             if (res == INT_MAX) res = level;
-            else if (level != res) return "NO";
+            else if (level != res) return "No";
         }
-        if (tmp->left && !tmp->right) return "NO";
-        if (tmp->right && !tmp->left) return "NO";
+        if (tmp->left && !tmp->right) return "No";
+        if (tmp->right && !tmp->left) return "No";
         if (tmp->left && tmp->right) {
             q.push({tmp->left, level + 1});
             q.push({tmp->right, level + 1});
         }
     }
-    return "YES";
+    return "Yes";
 }
 
 int main() {
@@ -103,4 +103,76 @@ int main() {
         }
         cout << isPerfectBinaryTree(root) << endl;
     }
+    return 0;
+}#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left, *right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+int n;
+
+void makeNode(Node *node, int b, char c) {
+    if (c == 'L') node->left = new Node(b);
+    else node->right = new Node(b);
+}
+
+void findNode(Node *node, int a, int b, int c) {
+    if (!node) return;
+    if (node->data == a) makeNode(node, b, c);
+    else {
+        findNode(node->left, a, b, c);
+        findNode(node->right, a, b, c);
+    }
+}
+
+string isPerfectBinaryTree(Node *node) {
+    if (!node) return "YES";
+    queue<pair<Node *, int> > q;
+    q.push({node, 1});
+    int res = INT_MAX;
+    while (!q.empty()) {
+        Node *tmp = q.front().first;
+        int level = q.front().second;
+        q.pop();
+        if (!tmp->left && !tmp->right) {
+            if (res == INT_MAX) res = level;
+            else if (level != res) return "No";
+        }
+        if (tmp->left && !tmp->right) return "No";
+        if (tmp->right && !tmp->left) return "No";
+        if (tmp->left && tmp->right) {
+            q.push({tmp->left, level + 1});
+            q.push({tmp->right, level + 1});
+        }
+    }
+    return "Yes";
+}
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d", &n);
+        int a, b;
+        char c;
+        Node *root = NULL;
+        while (n--) {
+            scanf("%d %d %c", &a, &b, &c);
+            if (!root) {
+                root = new Node(a);
+                makeNode(root, b, c);
+            } else findNode(root, a, b, c);
+        }
+        cout << isPerfectBinaryTree(root) << endl;
+    }
+    return 0;
 }

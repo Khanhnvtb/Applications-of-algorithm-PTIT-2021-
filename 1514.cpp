83ea64c66efb9c81e37892c392a4c33d98014b61
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-11-14 19:00:17 +0700

    Update 1514.cpp

--- 1514.cpp
+++ 1514.cpp
@@ -2,6 +2,47 @@
 // Created by khanh on 10/13/2021.
 //
 
+/*
+SỬA LẠI DÃY NGOẶC
+
+Bài làm tốt nhất
+Cho một xâu chỉ gồm các kí tự ‘(‘, ‘) và có độ dài chẵn. Hãy đếm số lượng dấu ngoặc cần phải đổi chiều ít nhất, sao cho xâu mới thu được là một dãy ngoặc đúng.
+
+Input:
+
+Dòng đầu tiên là số lượng bộ test T (T ≤ 20).
+
+Mỗi test gồm 1 xâu S có độ dài không vượt quá 100 000, chỉ gồm dấu ( và ).
+
+Output: 
+
+Với mỗi test, in ra đáp án tìm được trên một dòng.
+
+Ví dụ:
+
+Input:
+
+4
+
+))((
+
+((((
+
+(((())
+
+)(())(((
+
+Output
+
+2
+
+2
+
+1
+
+3
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;

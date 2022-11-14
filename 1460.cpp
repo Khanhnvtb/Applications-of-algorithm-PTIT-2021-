6b37ed16792cf28fb181f4e73d2d0ee9bd526f7a
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-11-14 18:12:21 +0700

    Update 1460.cpp

--- 1460.cpp
+++ 1460.cpp
@@ -2,6 +2,35 @@
 // Created by khanh on 10/12/2021.
 //
 
+/*
+BÀI 11. XÂU ĐỐI XỨNG 1
+Cho xâu ký tự str. Nhiệm vụ của bạn là tìm số phép chèn tối thiểu các ký tự vào str để str trở thành xâu đối xứng. Ví dụ: str =”ab” ta có số phép chèn tối thiểu là 1 để trở thành xâu đối xứng “aba” hoặc “bab”. Với xâu str=”aa” thì số phép chèn tối thiểu là 0. Với xâu str=”abcd” có số phép chèn tối thiểu là 3 để trở thành xâu “dcbabcd”
+
+Input:
+
+Dòng đầu tiên đưa vào số lượng bộ test T.
+Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test là một xâu ký tự được viết trên một dòng
+T, str thỏa mãn ràng buộc: 1≤T≤100;  1≤length(str)≤40.
+Output:
+
+Đưa ra kết quả mỗi test theo từng dòng.
+Ví dụ:
+
+Input
+
+3
+abcd
+aba
+geeks
+
+Output
+
+3
+0
+
+3
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;

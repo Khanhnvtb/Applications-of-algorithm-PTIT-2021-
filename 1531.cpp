e97dc1df5be31c46ffdc2addb45e861e697089f6
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-11-14 19:17:53 +0700

    Update 1531.cpp

--- 1531.cpp
+++ 1531.cpp
@@ -2,9 +2,50 @@
 // Created by Khanh Nguyen Van on 10/17/2021.
 //
 
-//
-// Created by Khanh Nguyen Van on 10/17/2021.
-//
+/*
+BÀI 18. TÍNH TỔNG
+
+Cho một xâu s. Với mỗi một xâu con X liên tiếp của s có độ dài bằng K, giá trị đặc biệt của nó được tính bằng giá trị của X trong hệ cơ số B modulo M.
+
+Nhiệm vụ của bạn là tính tổng giá trị đặc biệt của tất cả các xâu con của s có độ dài bằng K.
+
+Input:
+
+Dòng đầu tiên gồm xâu S có độ dài không quá 300 000 gồm các kí tự từ 0 – 9.
+
+Dòng tiếp theo là số nguyên K, B và M (1 <= K <= |s|, 2 <= B <= 10, 1 <= M <= 1000).
+
+Output: 
+
+In ra đáp án tìm được.
+
+Test ví dụ:
+
+Test 1
+
+Test 2
+
+Input:
+
+12212
+
+3 3 5
+
+Output:
+
+5
+
+Input:
+
+111101
+
+4 2 15
+
+Output:
+
+27
+*/
+
 
 #include <bits/stdc++.h>
 

de13cab6a190ffdd48aa9e41d3701c0bb8377405
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-11-14 18:13:31 +0700

    Update 1461.cpp

--- 1461.cpp
+++ 1461.cpp
@@ -2,6 +2,33 @@
 // Created by khanh on 10/12/2021.
 //
 
+/*
+BÀI 12. XÂU ĐỐI XỨNG 2
+
+Cho xâu ký tự S. Nhiệm vụ của bạn là tìm số phép loại bỏ ít nhất các ký tự trong S để S trở thành xâu đối xứng. Chú ý, phép loại bỏ phải bảo toàn tính trước sau của các ký tự trong S.
+
+Input:
+
+Dòng đầu tiên đưa vào số lượng bộ test T.
+Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test là một xâu ký tự được viết trên một dòng
+T, str thỏa mãn ràng buộc: 1≤T≤100;  1≤length(S)≤100.
+Output:
+
+Đưa ra kết quả mỗi test theo từng dòng.
+Ví dụ:
+
+Input
+
+2
+aebcbda
+geeksforgeeks
+
+Output
+
+2
+8
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 

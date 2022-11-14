142951d0c2ce37bb893c82a024f1bb32a93b65f9
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-11-14 19:02:26 +0700

    Update 1516.cpp

--- 1516.cpp
+++ 1516.cpp
@@ -2,12 +2,44 @@
 // Created by Khanh Nguyen Van on 10/15/2021.
 //
 
+/*
+SO SÁNH BIỂU THỨC
+Cho P1, P2 là hai biểu thức đúng chỉ bao gồm các ký tự mở ngoặc ‘(’ hoặc đóng ngoặc ‘)’ và các toán hạng in thường. Nhiệm vụ của bạn là định xem P1 và P2 có giống nhau hay không.
+
+Input:
+
+Dòng đầu tiên đưa vào số lượng bộ test T;
+Những dòng tiếp theo mỗi dòng đưa vào một bộ test. Mỗi bộ test gồm hai dòng: dòng thứ nhất đưa vào P1, dòng tiếp theo đưa vào P2.
+Output:
+
+Đưa ra kết quả mỗi test theo từng dòng.
+Ràng buộc:
+
+T, P thỏa mãn ràng buộc: 1≤T≤100; 1≤length(P) ≤100.
+Ví dụ:
+
+ 
+
+Input
+
+2
+-(a+b+c)
+-a-b-c
+a-b-(c-d)
+a-b-c-d
+
+Output
+
+YES
+NO
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
 string str1, str2;
 
-string convert(string str) { //hàm chuyển đổi đã làm ở bài DSA07005	BIỂU THỨC TƯƠNG ĐƯƠNG
+string convert(string str) {
     stack <int> s;
     string tmp = "";
     for (int i = 0; i < str.length (); i++)

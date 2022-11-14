128a6bf532dc271ab16c6d25dce0066fb33bdd2d
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-11-14 19:19:06 +0700

    Update 1532.cpp

--- 1532.cpp
+++ 1532.cpp
@@ -2,6 +2,53 @@
 // Created by khanh on 10/14/2021.
 //
 
+/*
+BÀI 19. BỘI SỐ LỚN NHẤT CỦA 3
+Cho dãy số A[] có N phần tử là các chữ số từ 0 đến 9. Nhiệm vụ của bạn là hãy chọn lấy một tổ hợp các phần tử và sắp xếp chúng sao cho thu được số lớn nhất chia hết cho 3.
+
+Nếu không tìm được số nào, in ra -1.
+
+Input:
+
+Dòng đầu tiên là số lượng bộ test T (1 <= N <= 50).
+
+Mỗi test bắt đầu bởi số nguyên N (1 <= N <= 100 000).
+
+Dòng tiếp theo gồm N số nguyên A[i] (0 <= A[i] <= 9).
+
+Output: 
+
+Với mỗi test, in ra đáp án tìm được trên một dòng.
+
+Test ví dụ:
+
+Input
+
+3
+
+3
+
+8 1 9
+
+5
+
+8 1 7 6 0
+
+Output
+
+2
+
+5 2
+
+ 
+
+981
+
+8760
+
+-1
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 

d3ec694264463df1e48506b75dfecc5a28e54c65
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-11-14 19:04:42 +0700

    Update 1519.cpp

--- 1519.cpp
+++ 1519.cpp
@@ -2,6 +2,38 @@
 // Created by khanh on 10/14/2021.
 //
 
+/*
+TÍNH GIÁ TRỊ BIỂU THỨC TIỀN TỐ
+
+Hãy viết chương trình tính toán giá trị của biểu thức tiền tố.
+
+Input:
+
+Dòng đầu tiên đưa vào số lượng bộ test T;
+Những dòng tiếp theo mỗi dòng đưa vào một bộ test. Mỗi bộ test là một biểu thức tiền tố exp. Các số xuất hiện trong biểu thức là các số đơn có 1 chữ số.
+Output:
+
+Đưa ra kết quả mỗi test theo từng dòng, chỉ lấy giá trị phần nguyên.
+Ràng buộc:
+
+T, exp thỏa mãn ràng buộc: 1≤T≤100; 2≤length(exp)≤20.
+Ví dụ:
+
+Input
+
+2
+
+-+8/632
+
+-+7*45+20
+
+Output
+
+8
+
+25
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 

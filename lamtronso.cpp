a8ca8664aa5ba9c221bec58691869d86e4901f62
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-11-15 19:35:19 +0700

    Update lamtronso.cpp

--- lamtronso.cpp
+++ lamtronso.cpp
@@ -2,6 +2,65 @@
 // Created by Khanh Nguyen Van on 10/16/2021.
 //
 
+/*
+LÀM TRÒN SỐ
+
+Bài làm tốt nhất
+Cho số nguyên dương không quá 9 chữ số. Hãy làm tròn số N theo quy tắc sau:
+
+Nếu N>10, làm tròn đến số hàng chục gần nhất
+Sau đó nếu kết quả lớn hơn 100 thì làm tròn đến số hàng trăm gần nhất
+Sau đó nếu kết quả lớn hơn 1000 thì làm trong đến số hàng nghìn gần nhất
+Cứ tiếp tục như vậy …
+Chú ý: Giá trị 5 sẽ được làm tròn lên.
+
+Input
+
+Dòng đầu ghi số bộ test (không quá 100)
+
+Mỗi bộ test ghi số N trên một dòng (N nguyên dương và không quá 9 chữ số)
+
+Output
+
+Với mỗi test, ghi ra kết quả làm tròn tương ứng trên một dòng.
+
+Ví dụ
+
+Input
+
+7
+
+15
+
+14
+
+5
+
+99
+
+12345678
+
+44444445
+
+1445
+
+Output
+
+20
+
+10
+
+5
+
+100
+
+10000000
+
+50000000
+
+2000
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;

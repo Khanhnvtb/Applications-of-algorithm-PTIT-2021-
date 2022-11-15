ce825ab749850cad9e29ecd4968ea701fa0460a7
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-11-15 19:33:45 +0700

    Update DSAKT110.cpp

--- DSAKT110.cpp
+++ DSAKT110.cpp
@@ -2,6 +2,34 @@
 // Created by Nguyen Van Khanh on 10/29/2021.
 //
 
+/*
+NHÀ KHÔNG KỀ NHAU
+Có N ngôi nhà trên một dãy phố, mỗi ngôi nhà chứa đựng một số lượng tài sản khác nhau. Một tên trộm muốn ăp cắp được nhiều nhất tài sản của dãy phố nhưng không muốn lấy tài sản của hai nhà kề nhau. Hãy cho biết, bằng cách đó tên trộm có thể đánh cắp được nhiều nhất bao nhiêu tài sản.
+
+Input:
+
+Dòng đầu tiên đưa vào số lượng bộ test T.
+Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm hai dòng: dòng thứ nhất là một số N là số lượng ngôi nhà; dòng tiếp theo đưa vào N số là tài sản tương ứng trong mỗi ngôi nhà; các số được viết cách nhau một vài khoảng trống.
+T, N, A[i] thỏa mãn ràng buộc: 1≤T≤100;  1≤N ≤106; 1≤A[i] ≤107.
+Output:
+
+Đưa ra kết quả mỗi test theo từng dòng.
+Ví dụ:
+
+Input
+
+2
+6
+5 5 10 100 10 5
+4
+3 2 7 10
+
+Output
+
+110
+13
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;

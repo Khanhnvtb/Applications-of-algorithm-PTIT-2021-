d95c8e7b5115328ed6104fc365963aa420cb4df0
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-12-27 09:45:09 +0700

    Update noiday2.cpp

--- noiday2.cpp
+++ noiday2.cpp
@@ -2,6 +2,32 @@
 // Created by Nguyen Van Khanh on 10/23/2021.
 //
 
+/*
+Cho N sợi dây với độ dài khác nhau được lưu trong mảng A[]. Nhiệm vụ của bạn là nối N sợi dây thành một sợi sao cho tổng chi phí nối dây là nhỏ nhất. Biết chi phí nối sợi dây thứ i và sợi dây thứ j là tổng độ dài hai sợi dây A[i] và A[j].
+
+Input
+
+Dòng đầu ghi số bộ test T (T<10). Mỗi bộ test gồm 2 dòng. Dòng đầu tiên là số nguyên N (N ≤ 2*106).
+
+Dòng tiếp theo gồm N số nguyên dương c[i] ( 1 ≤ A[i] ≤ 109).
+
+Output
+
+In ra đáp án của bộ test trên từng dòng, theo modulo 109+7.
+
+Ví dụ:
+
+Input:
+
+7
+
+2 4 1 2 10 2 3
+
+Output
+
+59
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;

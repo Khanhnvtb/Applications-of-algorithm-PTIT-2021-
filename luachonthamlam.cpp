265527042f2c66880a76de1604bd1cd5138d2081
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-12-27 08:57:36 +0700

    Update luachonthamlam.cpp

--- luachonthamlam.cpp
+++ luachonthamlam.cpp
@@ -2,6 +2,38 @@
 // Created by Khanh Nguyen Van on 10/16/2021.
 //
 
+/*
+Cho hai số nguyên dương N và S. Hãy lựa chọn các chữ số phù hợp để tạo ra số nhỏ nhất và số lớn nhất có N chữ số sao cho tổng chữ số đúng bằng S.
+
+Input
+
+Chỉ có một dòng ghi hai số N và S. (0 < N <= 100; 0 <= S <= 900)
+
+Output
+
+Ghi ra hai số nhỏ nhất và lớn nhất tìm được, cách nhau một khoảng trống.
+
+Nếu không thể tìm được thì ghi ra “-1 -1”
+
+Ví dụ
+
+Input
+
+3 20
+
+299 992
+
+2 900
+
+Output
+
+-1 -1
+
+3 0
+
+-1 -1
+*/
+
 #include<bits/stdc++.h>
 
 using namespace std;

7f4b9b63410246c6b3bebfae45fd372d2152d286
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-12-27 09:31:17 +0700

    Update quanvuatrenbanco.cpp

--- quanvuatrenbanco.cpp
+++ quanvuatrenbanco.cpp
@@ -2,6 +2,33 @@
 // Created by Khanh Nguyen Van on 10/17/2021.
 //
 
+/*
+Trên bàn cờ vua kích thước 8*8 thì quân vua được phép di chuyển đến cả 8 ô liền kề theo cả đường dọc và đường chéo. Tất nhiên quân vua sẽ không thể di chuyển được ra ngoài bàn cờ.
+
+Cho 2 ô trên bàn cờ gọi là ô bắt đầu và ô kết thúc. Hãy tính xem quân vua cần ít nhất bao nhiêu bước để di chuyển từ ô bắt đầu đến ô kết thúc.
+
+Input
+
+Có 2 cặp số nguyên s1,s2 và f1,f2 lần lượt và vị trí ô bắt đầu và ô kết thúc. Các vị trí đảm bảo nằm trong phạm vi bàn cờ.
+
+Output
+
+Số bước đi ít nhất của quân vua
+
+Ví dụ
+
+Input
+
+4 3 1 6
+
+5 5 5 6
+
+Output
+3
+
+1
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;

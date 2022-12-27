eb05a92383d50c1bd9c25cbe354d32cd18869a58
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-12-27 09:34:47 +0700

    Update khonglienthongvoidinh1.cpp

--- khonglienthongvoidinh1.cpp
+++ khonglienthongvoidinh1.cpp
@@ -2,6 +2,44 @@
 // Created by Nguyen Van Khanh on 11/14/2021.
 //
 
+/*
+Cho đồ thị vô hướng G có N đỉnh, M cạnh. 
+
+Hãy liệt kê các đỉnh không cùng thành phần liên thông với đỉnh 1.
+
+Input
+
+Dòng đầu ghi 2 số N và M (0 < N < 300; 1 <= M <= N*(N-1)/2)).
+
+Tiếp theo là M dòng, mỗi dòng ghi một cạnh của đồ thị. Các cạnh được liệt kê với thứ tự bất kỳ.
+
+Output
+
+Ghi ra các đỉnh không liên thông với đỉnh 1 theo thứ tự tăng dần, mỗi dòng ghi một đỉnh. Nếu không có đỉnh nào thì ghi ra số 0.
+
+Ví dụ
+
+Input
+
+6 4
+
+1 3
+
+2 3
+
+1 2
+
+4 5
+
+Output
+
+4
+
+5
+
+6
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;

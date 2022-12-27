d812d3aeda399ee5b78d458267129903ac59912e
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-12-27 09:20:00 +0700

    Update xauconlonnhat.cpp

--- xauconlonnhat.cpp
+++ xauconlonnhat.cpp
@@ -2,6 +2,34 @@
 // Created by Nguyen Van Khanh on 10/31/2021.
 //
 
+/*
+Xâu con của một xâu ký tự S được tạo ra bằng cách lấy một hoặc nhiều ký tự trong S và giữ nguyên thứ tự ban đầu.
+
+Cho xâu S chỉ bao gồm các chữ cái viết thường. Hãy in ra xâu con có thứ tự từ điển là lớn nhất.
+
+Input
+
+Chỉ có xâu ký tự S, độ dài không quá 100000. Không có khoảng trống.
+
+Output
+
+Ghi ra xâu con có thứ tự từ điển lớn nhất.
+
+Ví dụ
+
+Input
+
+ababba
+
+abbcbccacbbcbaaba
+
+Output
+
+bbba
+
+cccccbba
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;

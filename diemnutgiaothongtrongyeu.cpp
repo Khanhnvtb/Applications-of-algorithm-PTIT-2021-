fd8d76fece3050ed1175a3884bded6fba33160e3
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-12-27 09:33:07 +0700

    Update diemnutgiaothongtrongyeu.cpp

--- diemnutgiaothongtrongyeu.cpp
+++ diemnutgiaothongtrongyeu.cpp
@@ -1,3 +1,38 @@
+/*
+Một thành phố có N điểm nút giao thông. Các tuyến đường hai chiều được thiết kế giúp cho người dân có thể đi từ một nút bất kỳ đến tất cả các nút còn lại.
+
+Những nút giao thông trọng yếu được định nghĩa là nút giao thông mà nếu các con đường đến nó đều bị chặn thì thành phố sẽ bị chia cắt, tức là khi đó sẽ có những cặp điểm nút không thể đi đến nhau được nữa.
+
+Giả sử các điểm nút giao thông được đánh số từ 1 đến N. Hãy liệt kê các nút giao thông trọng yếu theo thứ tự tăng dần.
+
+Input
+
+Dòng đầu là số bộ test (không quá 100)
+Mỗi bộ test bắt đầu với số nút giao thông N (không quá 1000) và số tuyến đường M.
+Tiếp theo là một dòng có M cặp số mô tả các tuyến đường hai chiều trong thành phố.
+Output
+
+Dòng đầu ghi ra số lượng điểm nút giao thông trọng yếu
+
+Dòng thứ 2 lần lượt liệt kê các nút giao thông trọng yếu theo thứ tự tăng dần
+
+Ví dụ
+
+Input
+
+1
+
+5 5
+
+1 2 1 3 2 3 2 5 3 4
+
+Output
+
+2
+
+2 3
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;

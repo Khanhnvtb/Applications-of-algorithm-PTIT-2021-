ab8ad98c144945688943827a5a0c9e68ff88a006
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-12-27 09:36:42 +0700

    Update lietkexaukytu.cpp

--- lietkexaukytu.cpp
+++ lietkexaukytu.cpp
@@ -2,6 +2,50 @@
 // Created by Khanh Nguyen Van on 10/17/2021.
 //
 
+/*
+Cho chữ cái c in hoa (‘A’ < c < ’K’) và số nguyên K (0 < K < (c – ‘A’)).
+
+Hãy tìm cách  liệt kê tất cả các xâu ký tự khác nhau được tạo ra bởi các chữ cái tính từ ‘A’ đến ký tự c. Các ký tự được phép lặp lại nhưng không tính các xâu là hoán vị của xâu nào đó đã liệt kê trước đó.
+
+Xem ví dụ để hiểu thêm yêu cầu đề bài.
+
+Input
+
+Chỉ có một dòng ghi chữ cái c và số nguyên K thỏa mãn ràng buộc đề bài.
+
+Output
+
+Ghi ra lần lượt các xâu ký tự kết quả theo thứ tự từ điển, mỗi xâu trên một dòng.
+
+Ví dụ
+
+Input
+
+D 2
+
+Output
+
+AA
+
+AB
+
+AC
+
+AD
+
+BB
+
+BC
+
+BD
+
+CC
+
+CD
+
+DD
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;

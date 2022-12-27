7bbb0910c9274f4c0bab2814682047d9052447a2
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-12-27 09:15:42 +0700

    Update hopvagiaocua2taphoptu.cpp

--- hopvagiaocua2taphoptu.cpp
+++ hopvagiaocua2taphoptu.cpp
@@ -2,6 +2,37 @@
 // Created by Nguyen Van Khanh on 10/31/2021.
 //
 
+/*
+Trong lập trình cơ bản, một từ được hiểu là một dãy ký tự liên tiếp không chứa khoảng trống, dấu tab hoặc dấu xuống dòng.
+
+
+Hãy xác định tập hợp các từ khác nhau trong một xâu ký tự, sau khi đã chuyển hết về dạng chữ thường. Sau đó nhập hai dòng ký tự và hiển thị hợp và giao của hai tập từ tương ứng.
+
+Input
+
+Chỉ có 2 dòng, mỗi dòng có độ dài không quá 1000 ký tự.
+
+Output
+
+Dòng 1 ghi hợp của 2 tập từ theo thứ tự từ điển
+
+Dòng 2 ghi giao của 2 tập từ theo thứ tự từ điển.
+
+Ví dụ
+
+Input
+
+Lap trinh huong doi tuong
+
+Ngon ngu lap trinh C++
+
+Output
+
+c++ doi huong lap ngon ngu trinh tuong
+
+lap trinh
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;

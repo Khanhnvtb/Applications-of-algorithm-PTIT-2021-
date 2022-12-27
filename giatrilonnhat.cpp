46ba552006f75fd029189e4bcb5f7fc1738946fa
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-12-27 09:21:44 +0700

    Update giatrilonnhat.cpp

--- giatrilonnhat.cpp
+++ giatrilonnhat.cpp
@@ -2,6 +2,38 @@
 // Created by Khanh Nguyen Van on 10/17/2021.
 //
 
+/*
+Cho dãy số nguyên A[] có N phần tử.
+
+Gọi f(i,j) = |ai| + |ai+1| + … + |aj|
+Goij g(i,j) = ai + ai+1 + … + aj
+Với tất cả các cặp 1 ≤ i ≤ j ≤ N.
+
+Hãy tính giá trị lớn nhất của f(i,j) + g(i,j).
+
+Input
+
+Dòng đầu ghi số N (1 ≤ N ≤ 50000)
+
+Dòng thứ 2 ghi N số nguyên của dãy A[]
+
+Output
+
+Ghi ra giá trị lớn nhất của f(i,j) + g(i,j)
+
+Ví dụ
+
+Input
+
+5
+
+-3 5 -10 8 -2
+
+Output
+
+26
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;

ead07cd636902e1a51e178358ca180a776f24105
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-12-27 08:59:20 +0700

    Update dayconcotongnguyento.cpp

--- dayconcotongnguyento.cpp
+++ dayconcotongnguyento.cpp
@@ -2,6 +2,46 @@
 // Created by Khanh Nguyen Van on 10/16/2021.
 //
 
+/*
+Cho dãy số A[] có N phần tử là các số nguyên dương khác nhau từng đôi một. Hãy sắp xếp dãy theo thứ tự giảm dần, sau đó liệt kê tất cả các dãy con của A[] có tổng các phần tử là số nguyên tố.
+
+Input
+
+Dòng đầu ghi số bộ test, mỗi test có 2 dòng:
+
+Dòng đầu ghi số N (2 < N <15)
+Dòng thứ 2 ghi N số của dãy A[], các số đều nguyên dương, nhỏ hơn 100 và khác nhau từng đôi một.
+Output
+
+Với mỗi test, liệt kê tất cả các dãy con có tổng các phần tử là số nguyên tố theo, mỗi dãy con trên một dòng. 
+
+Ví dụ
+
+Input
+
+1
+
+4
+
+3 2 5 4
+
+Output
+
+2
+
+3
+
+3 2
+
+4 3
+
+5
+
+5 2
+
+5 4 2
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;

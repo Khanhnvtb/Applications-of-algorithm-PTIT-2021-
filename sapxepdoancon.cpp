df7d26bc6e388a091e1f7c3075dc6a6582fa991a
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-12-27 09:18:23 +0700

    Update sapxepdoancon.cpp

--- sapxepdoancon.cpp
+++ sapxepdoancon.cpp
@@ -2,9 +2,46 @@
 // Created by Nguyen Van Khanh on 10/31/2021.
 //
 
-//
-// Created by Nguyen Van Khanh on 10/31/2021.
-//
+/*
+Cho dãy số nguyên A có N phần tử. Hãy đếm số lượng chỉ số M < N thỏa mãn: nếu sắp xếp đoạn con (A1,…,AM) và (AM+1, …, AN) theo thứ tự tăng dần thì ta được dãy số A tăng dần.
+
+Input
+
+Dòng đầu ghi số bộ test T
+
+Mỗi bộ test gồm 2 dòng:
+
+Dòng đầu ghi số N (2 ≤ N ≤ 105)
+Dòng thứ hai: ghi N số của dãy A (|Ai| ≤ 109).
+Output
+
+Với mỗi bộ test:
+
+Dòng đầu ghi ra số K là số lượng chỉ số M tìm được
+Dòng thứ 2 ghi ra K giá trị chỉ số thỏa mãn theo thứ tự tăng dần. Nếu K = 0 thì dòng này bỏ trống.
+Ví dụ
+
+Input
+
+2
+
+2
+
+2 1
+
+5
+
+2 1 3 5 4
+
+0
+
+Output
+
+2
+
+2 3
+*/
+
 
 #include <bits/stdc++.h>
 

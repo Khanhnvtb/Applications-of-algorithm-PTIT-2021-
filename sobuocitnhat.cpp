5863414577a187684f7edc7b8bcc6f2364c4aeea
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2023-01-02 06:19:33 +0700

    Update sobuocitnhat.cpp

--- sobuocitnhat.cpp
+++ sobuocitnhat.cpp
@@ -41,7 +41,7 @@ void solve() {
     for (int i = 0; i < n; i++) {
         dp[i] = 1;
         for (int j = 0; j < i; j++)
-            if (a[i] > a[j]) dp[i] = max(dp[i], dp[j] + 1);
+            if (a[i] >= a[j]) dp[i] = max(dp[i], dp[j] + 1);
         res = max(res, dp[i]);
     }
     printf("%d\n", n - res);
